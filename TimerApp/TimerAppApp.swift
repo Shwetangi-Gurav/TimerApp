928ca1362f4ba7d0853430558b299f585e2111a0
Shwetangi <shwetangi@saaspay.app>
2024-01-05 23:37:15 +0530

    Initial Commit

---
+++ TimerApp/TimerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerAppApp.swift
+//  TimerApp
+//
+//  Created by Shwetangi Gurav on 05/01/24.
+//
+
+import SwiftUI
+
+@main
+struct TimerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
